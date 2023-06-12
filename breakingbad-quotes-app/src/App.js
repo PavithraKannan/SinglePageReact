5affb1d9714dd26ac3b321c312c9c78e81b1424e
PavithraKannan <paaveethra@gmail.com>
2023-06-12 12:32:56 +0300

    Adding Styles

--- breakingbad-quotes-app/src/App.js
+++ breakingbad-quotes-app/src/App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const bull = (
   <Box
@@ -31,11 +36,13 @@ function App() {
   }, []);
 
   return (
+    <Card sx={{ minWidth: 275 }}>
     <div className="App">
       <h1>Breaking Bad Quotes</h1>
       <p>{quote}</p>
       <button onClick={ handleRefreshClick }>Next</button>
     </div>
+    </Card>
   );
 }
 export default App;
