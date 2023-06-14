bddd4fd66de33c5160e56442a7ab2447de11e46d
PavithraKannan <paaveethra@gmail.com>
2023-06-14 16:03:21 +0300

    Favorite function update

--- breakingbad-quotes-app/src/App.js
+++ breakingbad-quotes-app/src/App.js
@@ -10,6 +10,8 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { SliderValueLabel } from '@mui/material';
 
+let nextId = 0;
+
 const bull = (
   <Box
     component="span"
@@ -19,20 +21,19 @@ const bull = (
 
 function App() {
   const [quote, setQuote] = useState('');
+  const [fav, setFav] = useState([]) ;
   const [counter, setCounter] = useState(0);
 
   const addFavorites = () => {
-    
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    const quoteElement = <p>{quote}</p>
-    root.render(quoteElement);
-  //alert(quote);
+    setFav([
+      ...fav,
+      { id: nextId++, quote: quote }
+    ]);
   }
 
   const deleteFavorites = () => {
     alert("Cleared the quotes from the list");
   }
-
 
   useEffect(() => {
     const fetchQuote = async () => {
@@ -57,15 +58,20 @@ function App() {
       {quote==='' && <p><Box sx={{ display: 'flex' }}> <CircularProgress /></Box></p>}
       {quote!=='' && <p>{quote}</p>}
       <p>Quote's counter: {counter}</p>
-      <p>
+      
       <CardActions>
          <Button variant="contained"  onClick={() => setQuote('')}>Next</Button> 
          <Button variant="contained" onClick={ addFavorites }>Add to favorites</Button> 
-         <Button variant="contained" onClick={ deleteFavorites }>Clear my favorites</Button> 
+        <Button variant="contained" onClick={ deleteFavorites }>Clear my favorites</Button>
       </CardActions>
-      </p>
+
       <div>
         <h3>My Favorites</h3>
+         <ul> 
+          {fav.map(fav => (
+            <li key={fav.id}>{fav.quote}</li>
+          ))} 
+        </ul>
       </div>
     </div>
     </Card>
